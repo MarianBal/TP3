213343179d72d2f5620112ee59a0838aeee77d4d
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-05-05 19:54:10 -0300

    M
    Menu hamburguesa :)

--- javascript/tpTres.js
+++ javascript/tpTres.js
@@ -1,1 +1,8 @@
 console.log('Hola Mundo')
+
+const hamb = document.getElementById('hamburguesa')
+const contenedor = document.getElementsByClassName("contenedor")[0]
+console.log(contenedor)
+
+ const noContenedor = () => contenedor.classList.add('noVisible');
+
