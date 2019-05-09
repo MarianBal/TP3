5829c3c8666283a6d3dae999fe07e934316021c7
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-05-09 20:32:55 -0300

    M

--- javascript/tpTres.js
+++ javascript/tpTres.js
@@ -7,9 +7,13 @@ const noContenedor = () => contenedor.classList.add('noVisible');
 
 const api= `https://api.themoviedb.org/3/movie/550?api_key=8bdfee1cadeaa7c6f8c489f17f927c3d`
 
-fetch (api)
+const popular = `https://api.themoviedb.org/3/movie/popular?api_key=${api}&language=en-US&page=1`
+
+fetch (popular)
     .then(res => res.json())
     .then(movie => console.log(movie))
+
+
 
 
 
