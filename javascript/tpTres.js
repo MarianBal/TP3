e8528374276a4ff0aecd7517205ebda74139eb7b
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-05-09 20:28:26 -0300

    M

--- javascript/tpTres.js
+++ javascript/tpTres.js
@@ -2,7 +2,14 @@ console.log('Hola Mundo')
 
 const hamb = document.getElementById('hamburguesa')
 const contenedor = document.getElementsByClassName("contenedor")[0]
-console.log(contenedor)
 
- const noContenedor = () => contenedor.classList.add('noVisible');
+const noContenedor = () => contenedor.classList.add('noVisible');
+
+const api= `https://api.themoviedb.org/3/movie/550?api_key=8bdfee1cadeaa7c6f8c489f17f927c3d`
+
+fetch (api)
+    .then(res => res.json())
+    .then(movie => console.log(movie))
+
+
 
