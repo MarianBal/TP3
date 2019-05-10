80d87e8b2d71b9a3606295e36a49dc5ceee95881
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-05-09 21:40:14 -0300

    M
    Al menos tengo título

--- javascript/tpTres.js
+++ javascript/tpTres.js
@@ -5,13 +5,39 @@ const contenedor = document.getElementsByClassName("contenedor")[0]
 
 const noContenedor = () => contenedor.classList.add('noVisible');
 
-const api= `https://api.themoviedb.org/3/movie/550?api_key=8bdfee1cadeaa7c6f8c489f17f927c3d`
+const pelis = document.getElementsByClassName('pelis')
 
-const popular = `https://api.themoviedb.org/3/movie/popular?api_key=${api}&language=en-US&page=1`
+let i=0;
+
+
+
+const apiKey= `8bdfee1cadeaa7c6f8c489f17f927c3d`;
+let paginaActual = 1;
+const popular =`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${paginaActual}`
+const topRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
+const upcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`
+const nowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
+
+const dire = `https://image.tmdb.org/`
+
 
 fetch (popular)
     .then(res => res.json())
-    .then(movie => console.log(movie))
+    .then(movie => {
+
+        for (let i=0; i<5; i++){
+
+            console.log(movie.results[i])
+            
+            pelis[i].innerHTML = `<div class="imagen"><img src="${dire}${movie.results[i].poster_path}"/></div>
+                                  <div class="tituloPeli">${movie.results[i].title}</div>`
+        }
+
+    })
+
+
+
+
 
 
 
