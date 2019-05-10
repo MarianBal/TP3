a0b22f732443d9243601687c15a111a95e36b8e3
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-05-09 21:52:04 -0300

    M

--- javascript/tpTres.js
+++ javascript/tpTres.js
@@ -7,18 +7,14 @@ const noContenedor = () => contenedor.classList.add('noVisible');
 
 const pelis = document.getElementsByClassName('pelis')
 
-let i=0;
-
-
-
 const apiKey= `8bdfee1cadeaa7c6f8c489f17f927c3d`;
 let paginaActual = 1;
 const popular =`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${paginaActual}`
-const topRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
-const upcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`
-const nowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
+const topRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=${paginaActual}`
+const upcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&page=${paginaActual}`
+const nowPlaying =`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${paginaActual}`
 
-const dire = `https://image.tmdb.org/`
+const dire = `https://image.tmdb.org/t/p/w370_and_h556_bestv2`
 
 
 fetch (popular)
@@ -26,6 +22,48 @@ fetch (popular)
     .then(movie => {
 
         for (let i=0; i<5; i++){
+
+            console.log(movie.results[i])
+            
+            pelis[i].innerHTML = `<div class="imagen"><img src="${dire}${movie.results[i].poster_path}"/></div>
+                                  <div class="tituloPeli">${movie.results[i].title}</div>`
+        }
+
+    })
+
+fetch (topRated)
+    .then(res => res.json())
+    .then(movie => {
+
+        for (let i=5; i<10; i++){
+
+            console.log(movie.results[i])
+            
+            pelis[i].innerHTML = `<div class="imagen"><img src="${dire}${movie.results[i].poster_path}"/></div>
+                                  <div class="tituloPeli">${movie.results[i].title}</div>`
+        }
+
+    })
+
+fetch (upcoming)
+    .then(res => res.json())
+    .then(movie => {
+
+        for (let i=10; i<15; i++){
+
+            console.log(movie.results[i])
+            
+            pelis[i].innerHTML = `<div class="imagen"><img src="${dire}${movie.results[i].poster_path}"/></div>
+                                  <div class="tituloPeli">${movie.results[i].title}</div>`
+        }
+
+    })
+
+    fetch (nowPlaying)
+    .then(res => res.json())
+    .then(movie => {
+
+        for (let i=15; i<20; i++){
 
             console.log(movie.results[i])
             
